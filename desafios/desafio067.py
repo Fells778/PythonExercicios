6867c7123ab69b4caa4e68c39c26f844d1383352
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-10 10:15:14 -0300

    atualizando desafio 67

--- desafios/desafio067.py
+++ desafios/desafio067.py
@@ -1,7 +1,18 @@
+from time import sleep
+
 print("========== DESAFIO 67 =========")
 
-
-
+while True:
+    print("=-" * 20)
+    n = int(input("Digite um número para ver sua tabuada: "))
+    print("=-" * 20)
+    if n < 0:
+        print("Você digitou um número negativo, tente novamente!")
+        break
+    for c in range(1, 11):
+        multi = c * n
+        print(f"{n} x {c} = {multi}.")
+        sleep(0.25)
 
 print("=-" * 20)
 print("FIM DO CODIGO")
