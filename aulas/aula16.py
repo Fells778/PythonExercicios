2887faf761685220c9d54ebb7a305efde62936a5
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-16 20:38:33 -0300

    criação da aula 16
    Dando inicio ao mundo 3 do curso de python

---
+++ aulas/aula16.py
@@ -0,0 +1,25 @@
+lanche = ("Humbúrguer", "Suco", "Pizza", "Pudim", "Batata frita")
+# Tuplas são imutáveis...
+# lanche[1] = "refri" <- isso não funciona...
+# for comida in lanche:
+#    print(f"Eu vou comer {comida}")
+
+# for cont in range (0, len(lanche)):
+#    print(f"Vou comer {lanche[cont]} na posição {cont}.")
+
+# for pos, comida in enumerate(lanche):
+#    print(f"Eu vou comer {comida} na posção {pos}.")
+
+# O sorted deixa organizado...
+# print(sorted(lanche))
+
+# a = (1, 2, 3)
+# b = (4, 5, 6, 7)
+# c = a + b
+# b + a não é o msm que a + b.
+# print(c)
+
+# Duplas são imutáveis, mas se pode apagar umaa dupla inteira...
+# pessoa = ("Felipe", 22, "M", 80.00)
+# del(pessoa)
+# print(pessoa)
