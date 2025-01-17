7ace98e633bc2e98fb1e5d54971fb4c1b573b679
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-17 09:53:49 -0300

    criação do desafio073

---
+++ desafios/desafio073.py
@@ -0,0 +1,21 @@
+print("========== DESAFIO 73 =========")
+
+print("Classificação NBB, temporada: 2024-25")
+times = ("Minas", "FlaBasquete", "Brasília Basquete", "Unifacisa", "Pinheiros", "União Corinthias", "Vasco", "Bauru",
+         "Corinthias", "Franca", "São Jocé", "São Paulo", "Paulistano", "Pato Basquete", "Mogi", "Botafogo",
+         "Caxias do sul", "Basquete Cearense")
+
+print("============ Times ==============")
+print(times)
+print("=========== Os 5 primeiros são. ===========")
+print(times[0:5])
+print("=========== Os 4 últimos colocados são. =========")
+print(times[-4:])
+print("=========== Os times em ordem alfabética. ==========")
+print(sorted(times))
+print("=========== O 8° colodado é o =============")
+print(f"{times[7]}.")
+
+
+print("=-" * 30)
+print("FIM DO CODIGO")
