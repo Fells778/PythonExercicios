22c8ddb339957344dafcbff4d1227b145de3d019
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-16 21:07:50 -0300

    criando o desafio 72
    primeiro exercicio com tupla

---
+++ desafios/desafio072.py
@@ -0,0 +1,12 @@
+tupla = (
+"zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze",
+"quatorze", "quinze", "dezeseis", "dezesete", "dezoito", "dezenove", "vinte")
+
+while True:
+
+    n = int(input("Digite um núemro entre 0 e 20: "))
+    if 0 <= n <= 20:
+        break
+    print("Tente novamente com um número entre 0 e 20")
+
+print(f"Você digitou o número {tupla[n]}")
