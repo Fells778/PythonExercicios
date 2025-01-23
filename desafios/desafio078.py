f6de56d4a2da38618cc7b958cc366394d4b6d4f3
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-23 19:42:39 -0300

    começando desafio 78

---
+++ desafios/desafio078.py
@@ -0,0 +1,13 @@
+print("========== DESAFIO 78 =========")
+
+n = []
+v = ""
+for c in range(0,5):
+    n.append(int(input("Digite um valor: ")))
+
+print(f"O maior valor foi {max(n)} na posição: {v}.")
+print(f"O menor valor foi: {min(n)} na posição: {v}.")
+print("=-" * 30)
+
+print("=-" * 30)
+print("FIM DO CODIGO")
