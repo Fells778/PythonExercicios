24f43d93604b974c4382eb06b938777cdbfd61d1
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-24 09:57:38 -0300

    fim do desafio 78

--- desafios/desafio078.py
+++ desafios/desafio078.py
@@ -5,9 +5,17 @@ v = ""
 for c in range(0,5):
     n.append(int(input("Digite um valor: ")))
 
-print(f"O maior valor foi {max(n)} na posição: {v}.")
-print(f"O menor valor foi: {min(n)} na posição: {v}.")
 print("=-" * 30)
-
+print(F"Você digitou os valores: {n}")
+print(f"O maior valor foi {max(n)} na posição: ", end="")
+for i, p in enumerate(n):
+    if p == max(n):
+        print(f"{i}...", end="")
+print("")
+print(f"O menor valor foi: {min(n)} na posição: ", end="")
+for i, p in enumerate(n):
+    if p == min(n):
+        print(f"{i}...", end="")
+print("")
 print("=-" * 30)
 print("FIM DO CODIGO")
