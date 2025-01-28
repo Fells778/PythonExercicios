b0799b80d1ac3abda696f7c16a005d8bd3402b14
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-01-28 09:37:05 -0300

    atualização do desafio 84

--- desafios/desafio084.py
+++ desafios/desafio084.py
@@ -2,13 +2,19 @@ print("========== DESAFIO 84 =========")
 
 pessoas = list()
 dado = list()
-cont = 0
-
+maior = menor = 0
 while True:
     dado.append(str(input("Nome: ")))
-    dado.append(str(input("Peso: ")))
+    dado.append(float(input("Peso: ")))
+    if len(pessoas) == 0:
+        maior = menor = dado[1]
+    else:
+        if dado[1] > maior:
+            maior = dado[1]
+        elif dado[1] < menor:
+            menor = dado[1]
     pessoas.append(dado[:])
-    cont += 1
+    dado.clear()
 
     parar = " "
     while parar not in "SN":
@@ -19,8 +25,17 @@ while True:
     dado.clear()
 
 print("=-" * 30)
-print(f"No total foram digitados {cont} pessoas.")
-print(f"O maior peso foi de {max(pessoas[1])}kg. Peso de {pessoas[0]}")
-print(f"O menor peso foi de {min(pessoas[1])}kg. Peso de {pessoas[0]}")
+print(f"Lista: {pessoas}")
+print(f"No total foram digitados {len(pessoas)} pessoas.")
+print(f"O maior peso foi de {maior}kg. Peso de ", end="")
+for p in pessoas:
+    if p[1] == maior:
+        print(f"{p[0]}", end="")
+print()
+print(f"O menor peso foi de {menor}kg. Peso de ", end="")
+for p in pessoas:
+    if p[1] == menor:
+        print(f"{p[0]}", end="")
+print()
 print("=-" * 30)
 print("FIM DO CODIGO")
